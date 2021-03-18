b5ef4fec895d304a58df9d0a1636861a0dcd13bd
wahyuwinoto <email@wahyu-winoto.com>
2021-03-19 02:17:34 +0700

    form login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Board from '../components/Board.vue';
+import Login from '../components/LoginForm.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login,
   },
+  
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/playgame',
+    name: 'Board',
+    component: Board,
+  },
+  
+];
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+router.beforeEach((to, from ,next)=>{
+  if(to.name == 'Board' && !localStorage.currentUser){
+    next({name: 'Login'})
+  }else{
+    next()
+  }
 })
 
-export default router
+export default router;
