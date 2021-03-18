7077c7d0076301fe1d9c63708828089fc09a734d
haritsmedina <haritsmedina@gmail.com>
2021-03-18 19:49:56 +0700

    SocketConnected

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req,res) => {
+  res.send('<h1>CONNECTED</h1>')
+})
+//FOR IO ON
+io.on('connection', (socket)=>{
+  console.log('a user connected')
+})
+
+http.listen(port , () => {
+  console.log(`server listened on port ${port}`)
+})
